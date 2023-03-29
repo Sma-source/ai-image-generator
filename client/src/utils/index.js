b940f1f0d5fa81a1e9b81c210eb12b90ef593c1f
Sma-source <rabhi.smain@gmail.com>
2023-03-29 18:37:19 +0200

    create getRandomPrompt function

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,1 +1,10 @@
 import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length); // create random index from 1-49
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt); // checked that we dont get same randomPrompt two times in a row
+
+  return randomPrompt;
+}
