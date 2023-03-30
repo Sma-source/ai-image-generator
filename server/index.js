bbc95765d3c654d3686cbc03a335abaf823af5d0
Sma-source <rabhi.smain@gmail.com>
2023-03-30 19:32:13 +0200

    update index.js: init express and middleware

--- server/index.js
+++ server/index.js
@@ -3,3 +3,7 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 dotenv.config();
+
+const app = express(); // init express app
+app.use(cors()); // add additional middleware
+app.use(express.json({ limit: "50mb" })); // add additional middleware
