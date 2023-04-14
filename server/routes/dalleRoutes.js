f6bf81b1e0bc6ae1edcc85336054793a8b788faf
Sma-source <rabhi.smain@gmail.com>
2023-04-14 19:56:50 +0200

    update dalleRoutes.js:add config fnct

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,4 +6,8 @@ dotenv.config(); //populate env variable
 
 const router = express.Router(); // instance router
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 export default router;
