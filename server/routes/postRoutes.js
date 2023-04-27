b9602a6d05a91f68e8e070c804cb656698adccb3
Sma-source <rabhi.smain@gmail.com>
2023-04-27 18:08:47 +0200

    update postRoutes.js: add cloudinary config

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,10 @@ dotenv.config(); //populate env variable
 
 const router = express.Router(); // instance router
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export default router;
